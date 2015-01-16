15623fb0f53a1a8874f181d0caf238fe478b4dd7
Cliff Snyder <cliff.snyder@gmail.com>
2015-01-15 22:20:19 -0800

    c does not allow overloading

--- c/dice.h
+++ c/dice.h
@@ -2,8 +2,8 @@
 #define __DICE_H 1
 
 int roll();
-int roll(int n); // default: d6
-int roll(int n, int s); // nDs
-int roll(int n, int s, int m); // nDs+m
+int roll_n(int n); // default: d6
+int roll_ns(int n, int s); // nDs
+int roll_ns_m(int n, int s, int m); // nDs+m
 
 #endif

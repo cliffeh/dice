0564583ca10c207ee6b5d44e09ffde0e2fc1c6b5
Cliff Snyder <cliff.snyder@gmail.com>
2015-01-18 21:04:42 -0800

    start on c

--- c/dice.c
+++ c/dice.c
@@ -1,1 +1,25 @@
 #include "dice.h"
+
+#include <time.h>
+#include <stdlib.h>
+
+int roll()
+{
+  return rollndm(1, 6, 0);
+}
+
+int rolln(int n)
+{
+  return rollndm(n, 6, 0);
+}
+
+int rollnd(int n, int d)
+{
+  return rollndm(n, d, 0);
+}
+
+int rollndm(int n, int d, int m)
+{
+  // TODO impl
+}
+
--- c/dice.h
+++ c/dice.h
@@ -1,9 +1,10 @@
 #ifndef __DICE_H
 #define __DICE_H 1
 
-int roll();
-int roll_n(int n); // default: d6
-int roll_ns(int n, int s); // nDs
-int roll_ns_m(int n, int s, int m); // nDs+m
+int roll();                       // default: 1d6
+int rolln(int n);                 // default: nd6
+int rollnd(int n, int d);
+int rollndm(int n, int s, int m);
 
 #endif
+

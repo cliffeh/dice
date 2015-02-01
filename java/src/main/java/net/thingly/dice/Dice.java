a956d3847a4a784d88c1991c03e425cfb351bf9e
Cliff Snyder <cliff.snyder@gmail.com>
2015-01-31 22:39:49 -0800

    formatting

--- java/src/main/java/net/thingly/dice/Dice.java
+++ java/src/main/java/net/thingly/dice/Dice.java
@@ -22,6 +22,6 @@ public class Dice {
 	}
 
 	public static void main(String[] args) {
-		
+
 	}
 }
--- java/src/test/java/net/thingly/dice/DiceTest.java
+++ java/src/test/java/net/thingly/dice/DiceTest.java
@@ -7,48 +7,42 @@ import junit.framework.TestSuite;
 /**
  * Unit test for Dice.
  */
-public class DiceTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public DiceTest( String testName )
-    {
-        super( testName );
-    }
+public class DiceTest extends TestCase {
+	/**
+	 * Create the test case
+	 *
+	 * @param testName
+	 *            name of the test case
+	 */
+	public DiceTest(String testName) {
+		super(testName);
+	}
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( DiceTest.class );
-    }
+	/**
+	 * @return the suite of tests being tested
+	 */
+	public static Test suite() {
+		return new TestSuite(DiceTest.class);
+	}
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testRoll()
-    {
-        assertTrue( true );
-    }
-    
-    /**
-     * Rigourous Test :-)
-     */
-    public void testRollN()
-    {
-        assertTrue( true );
-    }
-    
-    /**
-     * Rigourous Test :-)
-     */
-    public void testRollND()
-    {
-        assertTrue( true );
-    }
+	/**
+	 * Rigourous Test :-)
+	 */
+	public void testRoll() {
+		assertTrue(true);
+	}
+
+	/**
+	 * Rigourous Test :-)
+	 */
+	public void testRollN() {
+		assertTrue(true);
+	}
+
+	/**
+	 * Rigourous Test :-)
+	 */
+	public void testRollND() {
+		assertTrue(true);
+	}
 }

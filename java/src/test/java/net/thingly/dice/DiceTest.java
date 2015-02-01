477b178a38c035dd63e019ded8d49aceeeb22a8e
Cliff Snyder <cliff.snyder@gmail.com>
2015-02-01 12:27:28 -0800

    playing with chi-square test

--- java/src/test/java/net/thingly/dice/DiceTest.java
+++ java/src/test/java/net/thingly/dice/DiceTest.java
@@ -8,6 +8,9 @@ import junit.framework.TestSuite;
  * Unit test for Dice.
  */
 public class DiceTest extends TestCase {
+
+	private int iter = 100000;
+
 	/**
 	 * Create the test case
 	 *
@@ -29,7 +32,19 @@ public class DiceTest extends TestCase {
 	 * Rigourous Test :-)
 	 */
 	public void testRoll() {
-		assertTrue(true);
+		// Hashtable<Integer, Double> probs = Prob.prob(1, 6);
+		int[] results = new int[6];
+		for (int i = 0; i < iter; i++) {
+			int r = Dice.roll();
+			assertTrue(r >= 1 && r <= 6);
+			results[r-1]++;
+		}
+		double chisq = 0;
+		double expected = ((double)iter)/6.0; 
+		for(int i = 0; i < results.length; i++) {
+			chisq += Math.pow(((double)results[i])-expected, 2)/expected;
+		}
+		System.out.println("chi squared: " + chisq);
 	}
 
 	/**

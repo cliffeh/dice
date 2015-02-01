ee13491406e04f6e2e7c0d5c105821d869966673
Cliff Snyder <cliff.snyder@gmail.com>
2015-01-31 23:04:03 -0800

    working main method

--- java/src/main/java/net/thingly/dice/Dice.java
+++ java/src/main/java/net/thingly/dice/Dice.java
@@ -1,6 +1,9 @@
 package net.thingly.dice;
 
+import java.util.Arrays;
 import java.util.Random;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Dice {
 	private static final Random random = new Random();
@@ -16,12 +19,29 @@ public class Dice {
 	static int[] roll(int n, int d) {
 		int[] result = new int[n];
 		for (int i = 0; i < n; i++) {
-			result[i] = random.nextInt() % d + 1;
+			result[i] = random.nextInt(d) + 1;
 		}
 		return result;
 	}
 
 	public static void main(String[] args) {
+		int n = 1, d = 6;
 
+		if (args.length != 1) {
+			System.err.println("usage: dice NdD");
+			System.exit(1);
+		}
+
+		Pattern p = Pattern.compile("((\\d+)(d(\\d+))?)");
+		Matcher m = p.matcher(args[0]);
+		if (!m.matches()) {
+			System.err.println("usage: dice NdD");
+			System.exit(1);
+		}
+
+		n = Integer.parseInt(m.group(2));
+		d = m.group(4) != null ? Integer.parseInt(m.group(4)) : d;
+
+		System.out.println(Arrays.toString(roll(n, d)));
 	}
 }

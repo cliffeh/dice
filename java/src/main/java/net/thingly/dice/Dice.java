c2d54787d0d86f38bc48120fff07a94377ffdc49
Cliff Snyder <cliff.snyder@gmail.com>
2015-01-31 22:00:44 -0800

    initial java commit

---
+++ java/src/main/java/net/thingly/dice/Dice.java
@@ -0,0 +1,27 @@
+package net.thingly.dice;
+
+import java.util.Random;
+
+public class Dice {
+	private static final Random random = new Random();
+
+	static int roll() {
+		return roll(1, 6)[0];
+	}
+
+	static int[] roll(int n) {
+		return roll(n, 6);
+	}
+
+	static int[] roll(int n, int d) {
+		int[] result = new int[n];
+		for (int i = 0; i < n; i++) {
+			result[i] = random.nextInt() % d + 1;
+		}
+		return result;
+	}
+
+	public static void main(String[] args) {
+		
+	}
+}
